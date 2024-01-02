b0636292d5e74bc1b525d356fc1839b3307bfa95
Rafael Ferreira  Melo <101290871+Rafael-FerreiraM@users.noreply.github.com>
2024-01-02 14:26:11 -0300

    Update TesteInterfaceUol.java

--- src/test/java/TesteInterfaceUol.java
+++ src/test/java/TesteInterfaceUol.java
@@ -1,30 +1,41 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TesteInterfaceUol {
 
       private WebDriver driver;
-      private static final String baseURL = "https://www.uol.com.br";
+      private static final String baseURL = "https://www.uol.com.br/";
+      
+      UolHomePage home;
 
     
     @Test
     @DisplayName("Quando eu acessar a página inicial, Então deve aparecer o título esperado")
     public void quandoAcessarHomePage_EntaoDeveAparecerOTitulo() {
 
-        homePage = new UolHomePage();
-          
-        String tituloEsperado = "Expulsos por obras esperam há 6 anos água da transposição na PB";
-          
+        //Entrar no site Uol
         driver.get(baseURL);  
-      
-      
 
-        String tituloClickado = 
+        //Instanciar o Page Objects
+        home = new UolHomePage(driver);
 
-        assertEquals(tituloClickado, tituloEsperado);
+        //Clicar no título da notícia
+        home.
+              
+        //Titulo esperado a ser encontrado
+        String tituloEsperado = "Expulsos por obras esperam há 6 anos água da transposição na PB";
+
+        //Título que foi econtrado
+        String tituloEncontrado = driver.getTitle();
+
+        //Verificação se o título encontrado é igual ao título esperado
+        assertEquals(tituloEsperado, tituloEncontrado);
 
      
     }
@@ -32,7 +43,7 @@ public class TesteInterfaceUol {
       @BeforeAll
       public void setup (){
             driver = new ChromeDriver();
-            driver.manage().timeouts().implicitWait(Duration.ofMillis(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10));
       }
 
       @AfterAll
